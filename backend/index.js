3a36e381d299230691f06c45b02b307ecc0de025
Amisha Singla <78135215+Amisha121102@users.noreply.github.com>
2024-03-30 21:00:14 +0000

    added real-time-chat-app

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+const axios = require("axios");
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "a169bc82-be6e-43e1-8e17-966cf41b4497" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
